59bdcee21d0754fcb1614b77719ff1b0ea237d80
Andrea200103 <2123200442@soy.utj.edu.mx>
2026-03-03 17:06:43 -0600

    Proyectos e invitaciones

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { api, setAuth } from "../api";
 import logo from '../assets/logo.png';
 
 export default function Login() {
-    const nav = useNavigate();
+    
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [show, setShow] = useState(false);
